1029f1a06142b273c9978224c00b23967c92e836
alexalreadytaken <alex.shemelev22@gmail.com>
2022-08-24 14:17:27 +0500

    user model

---
+++ models/user.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'json'
+
+class UnprocessableUserError < RuntimeError; end
+
+class UserNameAlreadyTakenError < RuntimeError; end
+
+class User
+  GENDERS = %i[fm m nb].freeze
+
+  attr_reader :name, :gender, :age, :validation_errors
+
+  def initialize(name, gender, age)
+    @name = name
+    @gender = gender
+    @age = age
+    valid?
+  end
+
+  def valid?
+    @validation_errors = []
+    @validation_errors << 'name length must be between 1 and 30' unless user.name.length.between?(1, 30)
+    @validation_errors << 'unknown gender' unless GENDERS.include?(user.gender)
+    @validation_errors << 'age must be positive' unless user.age.positive?
+    @validation_errors.empty?
+  end
+
+  def save
+    raise UnprocessableUserError unless valid?
+    raise UserNameAlreadyTakenError if name_already_taken?
+
+    REDIS_CONNECTION.set(@name, to_json)
+  end
+
+  def name_already_taken?
+    REDIS_CONNECTION.exists? @name
+  end
+
+  def to_json(*args)
+    {
+      'name' => @name,
+      'gender' => @gender,
+      'age' => @age
+    }.to_json(args)
+  end
+
+  # fixme?
+  def self.find_by_name(name)
+    json = REDIS_CONNECTION.get(name)
+    nil if json.nil?
+    User.json_create(json)
+  end
+
+  def self.json_create(json)
+    new(json['name'], json['gender'], json['age'])
+  end
+end
