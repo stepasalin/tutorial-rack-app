08ff31a805d6e8e48d5b335d5960c2737f6a9cd2
Stepan Salin <ssalin@artec-group.com>
2022-09-29 22:59:21 +0200

    frontend development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       posts: []
     }
   }
-componentDidMount() {
+
+  componentDidMount() {
     const url = "https://jsonplaceholder.typicode.com/posts";
     fetch(url)
     .then(response => response.json())
     .then(json => this.setState({ posts: json }))
   }
-render() {
+
+  someApiCall() {
+    fetch('/someapicall')
+    .then(response => response.json())
+    .then(json => this.setState({ posts: json }))
+  }
+
+  render() {
+    const { posts } = this.state;
     return (
-      <p>Hello world!</p>
+      <div className="container">
+        <div class="jumbotron">
+          <h1 class="display-4">Blog posts</h1>
+        </div>
+        <button onClick={this.someApiCall}>Letz go bitch</button>
+        {posts.map((post) => (
+          <div className="card" key={post.id}>
+            <div className="card-header">
+              #{post.id} {post.title}
+            </div>
+            <div className="card-body">
+              <p className="card-text">{post.body}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     );
   }
 }
