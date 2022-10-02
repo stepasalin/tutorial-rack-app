dab64a1b6c55dbab5163b8a2120ac99283ff13e9
Stepan Salin <ssalin@artec-group.com>
2022-10-02 17:34:00 +0200

    frontend somewhat works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
+import LoadingSpinner from "./spinner/LoadingSpinner";
+// import "./styles.css";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-      nameInputValue: '',
-      isLoading: false
-    };
+export default function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleNameInputChange = (event) => {
+    setInputValue(event.target.value)
   }
-  handleNameInputChange(event){
-    this.setState({ nameInputValue: event.target.value} )
-  }
-
-  sendRawUserRequest(){
-    this.setState( {isLoding: true} );
-    fetch(`/raw_user/${this.state.nameInputValue}`)
-    .then(response => response.json())
-    .then(json => this.setState( { user: json })) 
-    this.setState( {isLoding: false} );
-  }
-
-  render() {
-    const { nameInputValue } = this.state;
-    const { user } = this.state;
-    return (
-      <div className="container">
-        <div class="jumbotron">
-          <h1 class="display-4">{nameInputValue}</h1>
-        </div>
-        <button onClick={this.sendRawUserRequest.bind(this)}>Search</button>
-        <input
+  const handleFetch = () => {
+    setIsLoading(true);
+    fetch(`/raw_user/${inputValue}`)
+      .then((respose) => respose.json())
+      .then((response) => {
+         setUser(response)
+         setIsLoading(false)
+      })
+      .catch(() => {
+         setErrorMessage("Unable to fetch user");
+         setIsLoading(false);
+      });
+  };
+  const renderUser = (
+    <div className="userlist-container">
+      <ul>
+        <li>Name: {user.name}</li>
+        <li>Gender: {user.gender}</li>
+        <li>Age: {user.age}</li>
+      </ul>
+    </div>
+  );
+  return (
+    <div className="App">
+      <h1>Search for user: {inputValue}</h1>
+       <input
           type="text"
-          id="user_name"
-          name="user_name"
-          onChange={this.handleNameInputChange.bind(this)}
-          value={nameInputValue}
+          id="id"
+          name="name"
+          onChange={handleNameInputChange}
+          value={inputValue}
         />
-        <RenderUser />
-        {/* <div>
-          <div className="card-header">
-            <p className="card-text">Gender: {user.gender}</p>
-            <p className="card-text">Age: {user.age}</p>
-          </div>
-        </div> */}
-      </div>
-    );
-  }
+      {isLoading ? <LoadingSpinner /> : renderUser}
+      {errorMessage && <div className="error">{errorMessage}</div>}
+      <button onClick={handleFetch} disabled={isLoading}>
+        Fetch User
+      </button>
+    </div>
+  );
 }
-export default App;
