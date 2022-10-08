a7102c08976fb7b2415865dadd63621ac2790808
Stepan Salin <ssalin@artec-group.com>
2022-10-08 16:55:16 +0200

    Add User ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import GetUser from './components/GetUser/GetUser.js'
+import AddUser from './components/AddUser/AddUser.js'
 import React from "react";
 
 export default function App() {
   
   return (
-    <GetUser />
+    <>
+      <GetUser />
+      <AddUser />
+    </>  
   );
 }
