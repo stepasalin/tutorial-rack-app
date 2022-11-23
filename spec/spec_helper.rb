f508da40a6bb0c980060ec33c021cd0233e03564
R0$ <rosdotua@gmail.com>
2022-11-23 18:22:06 +0200

    ApplicationResponse added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,33 @@
+class ApplicationResponse
+  def initialize(response)
+    @response = response
+  end
+
+
+  def status
+    @response[0]
+  end
+
+
+  def body
+    @response[2][0]
+  end
+end
+
+
 class Environment
   def initialize(app)
     @app = app
   end
 
+
   def simulate_request(method, path, body)
-    @app.call({
+    response = @app.call({
       "REQUEST_METHOD" => method,
       "PATH_INFO" => path,
-      'rack.input' => StringIO.new(body)
-    })
+      "rack.input" => StringIO.new(body)
+      })
+
+    ApplicationResponse.new(response)
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,23 +13,14 @@ RSpec.describe UserController do
     REDIS_CONNECTION.flushall
   end
 
-  # after(:all) { User.delete_user(user.name) } # todo
-
   context 'user validation' do
     it 'creates new user' do
-      # unique data has to be generated each time
       req_body = user.user_data
-      # написать helper что будет принимать 4 аргумента и генерить response
-      resp = environment.simulate_request('POST', '/user/data', req_body)
-
-      response = ApplicationResponse.new(resp)
-      # [code, headers, [content]]
-      # response parses needed
+      response = environment.simulate_request('POST', '/user/data', req_body)
 
       expect(response.status).to eq 201
       expect(response.body).to eq req_body
 
-      # has to be smth like expect { User.find(...) }.to eq(..)
       expect { User.find(user.name) }.to eq(req_body)
 
       # сгенерить данные
