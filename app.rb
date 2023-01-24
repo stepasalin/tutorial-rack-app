611e45e0e5829df9822806794d335248df83425d
Stepan Salin <ssalin@artec-group.com>
2023-01-24 14:01:35 +0000

    refactor app to be able to test

---
+++ app.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rack'
+require 'pry'
+require 'json'
+require 'redis'
+require_relative 'models/user'
+require_relative 'helpers/redis_helper'
+require_relative 'controllers/user'
+require_relative 'views/user'
+require_relative 'helpers/HTML_page'
+
+class Application
+  def call(env)
+    req = Rack::Request.new(env)
+    controller = UserController.new(req)
+    if req.post? && req.path.start_with?('/user/new')
+      controller.create
+    elsif req.get? && req.path.start_with?('/user/html/')
+      controller.get_user_html
+    elsif req.post? && req.path.start_with?('/user/update')
+      controller.update
+    elsif req.delete? && req.path.start_with?('/user')
+      controller.delete
+    elsif req.get? && req.path.start_with?('/get_users')
+      controller.all_users_list
+    else
+      [404, {}, ["Sorry, dunno what to do about #{req.request_method} #{req.path}"]]
+    end
+  end
+end
