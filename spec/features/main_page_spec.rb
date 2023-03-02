7ab2581a5aedfb0bf02890e961362faba9ae0dac
Stepan Salin <ssalin@artec-group.com>
2023-03-02 14:29:05 +0100

    first test is ready - 2

--- spec/features/main_page_spec.rb
+++ spec/features/main_page_spec.rb
@@ -5,20 +5,23 @@ describe 'Base Page' do
   before(:each) { User.delete_all }
 
   it 'creates the user' do
-    Capybara.visit 'http://localhost:3000/'
+    visit 'http://localhost:3000/'
 
-    create_section = Capybara.find('div#root > div:nth-of-type(2)')
-    name_input = create_section.find('#name')
-    gender_input = create_section.find('#gender')
-    age_input = create_section.find('#age')
+    create_section = find('div[id="root"] > div:nth-of-type(2)')
+
+    name_input = create_section.find('input[id="name"]')
+    gender_input = create_section.find('input[id="gender"]')
+    age_input = create_section.find('input[id="age"]')
     create_button = create_section.find(:button, 'Create User')
+    # HELP: google <- 'capybara cheat sheet'
 
     name = 'vasya'
+
     name_input.set name
     age_input.set 123
     gender_input.set 'm'
     create_button.click
-    
+    # 99% = set + click
     sleep 6
 
     expect(User.find(name)).not_to be_nil
